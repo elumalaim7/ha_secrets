4bcf9f7eb5ba11adaef660913ad137736d52326a
Lynn Dong <ldong@nebulaworks.com>
2019-03-28 09:56:26 -0700

    deploy on multiple tf workspaces

--- kubeconfig.tf
+++ kubeconfig.tf
@@ -13,8 +13,8 @@ contexts:
 - context:
     cluster: kubernetes
     user: aws
-  name: aws
-current-context: aws
+  name: ${terraform.workspace}-aws
+current-context: ${terraform.workspace}-aws
 kind: Config
 preferences: {}
 users:
@@ -38,12 +38,22 @@ data "template_file" "kubeconfig" {
   template = "${local.kubeconfig}"
 }
 
+# Creates a kubeconfig file on your local machine 
 resource "null_resource" "kubeconfig" {
   provisioner "local-exec" {
-    command = "echo '${data.template_file.kubeconfig.rendered}' > $HOME/.kube/config"
+    command = "echo '${data.template_file.kubeconfig.rendered}' > $HOME/.kube/config.${terraform.workspace}"
   }
 
   depends_on = ["aws_autoscaling_group.demo"]
+}
+
+# aws command to automatically add kubeconfig information given an eks cluster name and region
+resource "null_resource" "kubeconfig2" {
+  provisioner "local-exec" {
+    command = "aws eks --region ${var.region} update-kubeconfig --alias ${terraform.workspace}-aws --name ${terraform.workspace}-${var.cluster_name}"
+  }
+
+  depends_on = ["aws_eks_cluster.demo"]
 }
 
 # Deploy ConfigMap for worker nodes w/demo-node arn to be added to the cluster under the following clusterroles: system:bootstrappers and system:nodes
